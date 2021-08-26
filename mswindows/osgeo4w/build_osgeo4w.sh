67a5b197d07e6ff80484cadd17aefc4f10268bde
ninsbl <stefan.blumentrath@nina.no>
2021-08-26 08:30:57 +0200

    add include/c++/10.3.0

--- mswindows/osgeo4w/build_osgeo4w.sh
+++ mswindows/osgeo4w/build_osgeo4w.sh
@@ -29,7 +29,7 @@ export ARCH=x86_64-w64-mingw32
 export C_INCLUDE_PATH=".:${OSGEO4W_ROOT_MSYS}/include:${SRC}/dist.${ARCH}/include:/c/msys64/mingw64/${ARCH}/include:/c/msys64/mingw64/include"
 export CPLUS_INCLUDE_PATH=".:${OSGEO4W_ROOT_MSYS}/include:${SRC}/dist.${ARCH}/include:/c/msys64/mingw64/${ARCH}/include:/c/msys64/mingw64/include"
 export PYTHONHOME=${OSGEO4W_ROOT_MSYS}/apps/Python39
-export CPPFLAGS="-I/c/msys64/mingw64/${ARCH}/include -I/c/msys64/mingw64/include -I${OSGEO4W_ROOT_MSYS}/include -I${SRC}/dist.${ARCH}/include -I/usr/include"
+export CPPFLAGS="-I/c/msys64/mingw64/include/c++/10.3.0 -I/c/msys64/mingw64/${ARCH}/include -I/c/msys64/mingw64/include -I${OSGEO4W_ROOT_MSYS}/include -I${SRC}/dist.${ARCH}/include -I/usr/include"
 
 
 ./configure \
