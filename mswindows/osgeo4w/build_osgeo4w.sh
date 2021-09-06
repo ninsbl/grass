9c9b20b47a2f75c692c0e0d27c72416a29eaca98
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-09-07 00:09:40 +0200

    Update build_osgeo4w.sh

--- mswindows/osgeo4w/build_osgeo4w.sh
+++ mswindows/osgeo4w/build_osgeo4w.sh
@@ -24,7 +24,7 @@
 
 
 # compile
-export VCPATH="$(cygpath "C:/Program~1/Microsoft~1/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include")"
+export VCPATH="$(cygpath "C:/Program~2/Micros~1/2019/Enterprise/VC/Tools/MSVC/14.29.30133/include")"
 export PATH="${OSGEO4W_ROOT_MSYS}/bin:/usr/bin:/mingw64/bin"
 export ARCH=x86_64-w64-mingw32
 export C_INCLUDE_PATH=".:${OSGEO4W_ROOT_MSYS}/include:${SRC}/dist.${ARCH}/include:/c/msys64/mingw64/${ARCH}/include:/c/msys64/mingw64/include"
