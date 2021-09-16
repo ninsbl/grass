be5c0c2bade8d3baa6988f6aa91d2f7df894928b
Stefan Blumentrath <stefan.blumentrath@nina.no>
2021-09-16 22:48:27 +0200

    no ODBC

--- mswindows/osgeo4w/build_osgeo4w.sh
+++ mswindows/osgeo4w/build_osgeo4w.sh
@@ -64,7 +64,6 @@ export ARCH=x86_64-w64-mingw32
     --with-regex \
     --with-nls \
     --with-zstd \
-    --with-odbc \
     --with-cairo \
     --with-cairo-includes=${SRC}/include \
     --with-cairo-ldflags="-L${SRC}/mswindows/osgeo4w/lib -lcairo -lfontconfig" \
