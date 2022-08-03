a0d140f1c2ae0d4b3548100f6cd1b9fb6b49a975
ninsbl <stefan.blumentrath@gmx.de>
2022-08-03 13:08:01 +0200

    try lapack

--- mswindows/osgeo4w/build_osgeo4w.sh
+++ mswindows/osgeo4w/build_osgeo4w.sh
@@ -24,8 +24,8 @@ set -e
 
 
 # compile
-export PATH=${OSGEO4W_ROOT_MSYS}/bin:/usr/bin:/mingw64/bin
-export C_INCLUDE_PATH=".:${OSGEO4W_ROOT_MSYS}/include:${SRC}/dist.${ARCH}/include:/c/msys64/mingw64/include"
+export PATH=${OSGEO4W_ROOT_MSYS}/bin:/usr/bin:/d/msys64/mingw64/bin
+export C_INCLUDE_PATH=".:${OSGEO4W_ROOT_MSYS}/include:${SRC}/dist.${ARCH}/include:/d/msys64/mingw64/include"
 export PYTHONHOME=${OSGEO4W_ROOT_MSYS}/apps/Python39
 export ARCH=x86_64-w64-mingw32
 
@@ -47,7 +47,8 @@ export ARCH=x86_64-w64-mingw32
     --with-readline \
     --with-wxwidgets \
     --with-blas \
-    --with-lapack-includes=/mingw64/include/lapack \
+    --with-lapack \
+    --with-lapack-includes=/d/msys64/mingw64/include \
     --with-freetype \
     --with-freetype-includes=${OSGEO4W_ROOT_MSYS}/include/freetype2 \
     --with-proj-share=${OSGEO4W_ROOT_MSYS}/share/proj \
